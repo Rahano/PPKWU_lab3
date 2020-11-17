ce2d56ced171eae05841e65cb426c094c1f8dabc
Rahano <wojciechmuras@wp.pl>
2020-11-17 14:26:37 +0100

    Add mapping for getting events

--- src/main/java/ppkwu/weeiaCalendar/CalendarController.java
+++ src/main/java/ppkwu/weeiaCalendar/CalendarController.java
@@ -1,8 +1,16 @@
 package ppkwu.weeiaCalendar;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalendarController {
+
+    @RequestMapping(path = "/getEvents")
+    public String reverseString(){
+
+        return null;
+
+    }
 
 }
