34c9d8ec2a4d6f1fc65676ca5c3d2572ee12cdfa
Rahano <wojciechmuras@wp.pl>
2020-11-17 14:21:58 +0100

    Initialize project

---
+++ src/main/java/ppkwu/weeiaCalendar/WeeiaCalendarApplication.java
@@ -0,0 +1,13 @@
+package ppkwu.weeiaCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeiaCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeiaCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ppkwu/weeiaCalendar/WeeiaCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package ppkwu.weeiaCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeiaCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
