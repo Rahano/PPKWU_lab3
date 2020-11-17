93e6d7a891881386ab8a791525cee7e6bab1dc36
Rahano <wojciechmuras@wp.pl>
2020-11-17 14:43:56 +0100

    Connect to calendar using Jsoup

--- src/main/java/ppkwu/weeiaCalendar/CalendarController.java
+++ src/main/java/ppkwu/weeiaCalendar/CalendarController.java
@@ -1,17 +1,24 @@
 package ppkwu.weeiaCalendar;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class CalendarController {
 
     @RequestMapping(path = "/getEvents")
     public String reverseString(@RequestParam(value = "year") int year,
-            @RequestParam(value = "month") int month){
+            @RequestParam(value = "month") int month) throws IOException {
 
         String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + month;
+
+        Document document = Jsoup.connect(url).get();
+
 
         return null;
 
