f959c3b05c9daef2948e2939765c3073fb0c5564
Rahano <wojciechmuras@wp.pl>
2020-11-17 14:30:37 +0100

    Add input parameters for getEvents

--- src/main/java/ppkwu/weeiaCalendar/CalendarController.java
+++ src/main/java/ppkwu/weeiaCalendar/CalendarController.java
@@ -1,13 +1,15 @@
 package ppkwu.weeiaCalendar;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalendarController {
 
     @RequestMapping(path = "/getEvents")
-    public String reverseString(){
+    public String reverseString(@RequestParam(value = "year") int year,
+            @RequestParam(value = "month") int month){
 
         return null;
 
