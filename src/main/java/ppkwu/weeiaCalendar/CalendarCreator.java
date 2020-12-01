bac59b13970a493b118748697da35c53f1184612
Rahano <wojciechmuras@wp.pl>
2020-12-01 14:07:19 +0100

    Add UID to calendar and VEvents

--- src/main/java/ppkwu/weeiaCalendar/CalendarCreator.java
+++ src/main/java/ppkwu/weeiaCalendar/CalendarCreator.java
@@ -5,6 +5,7 @@ import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Uid;
 import net.fortuna.ical4j.model.property.Version;
 import org.jsoup.select.Elements;
 
@@ -15,6 +16,7 @@ public class CalendarCreator {
         calendar.getProperties().add(new ProdId("-//Kalendarz WEEIA//"));
         calendar.getProperties().add(Version.VERSION_2_0);
         calendar.getProperties().add(CalScale.GREGORIAN);
+        calendar.getProperties().add(new Uid(   "WeeiaCalendarExample"));
         calendar = addEvents(calendar, event_names, event_days, month);
 
         System.out.println(calendar);
@@ -27,6 +29,7 @@ public class CalendarCreator {
             cal.set(java.util.Calendar.DAY_OF_MONTH, Integer.parseInt(event_days.get(i).text()));
 
             VEvent event = new VEvent(new Date(cal.getTime()), event_names.get(i).text());
+            event.getProperties().add(new Uid(   "WeeiaCalendarExample" + i));
 
             calendar.getComponents().add(event);
         }
