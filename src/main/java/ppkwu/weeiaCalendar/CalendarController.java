b4904c1d8d450ec6e9196fb2661f62aeb8b01d2c
Rahano <wojciechmuras@wp.pl>
2020-12-01 14:45:37 +0100

    Refactor Controller class

--- src/main/java/ppkwu/weeiaCalendar/CalendarController.java
+++ src/main/java/ppkwu/weeiaCalendar/CalendarController.java
@@ -1,8 +1,6 @@
 package ppkwu.weeiaCalendar;
 
-import ppkwu.weeiaCalendar.CalendarCreator;
 import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +13,8 @@ import java.net.URISyntaxException;
 @RestController
 public class CalendarController {
 
-    @RequestMapping(path = "/getEvents")
-    public String GetWEEIACalendar(@RequestParam(value = "year") int year,
+    @RequestMapping(path = "/getCalendar")
+    public void GetWEEIACalendar(@RequestParam(value = "year") int year,
             @RequestParam(value = "month") int month, HttpServletResponse response) throws IOException, URISyntaxException {
 
         String url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + year + "&miesiac=" + String.format("%02d", month);
@@ -25,8 +23,5 @@ public class CalendarController {
 
         CalendarCreator calendarCreator = new CalendarCreator();
         calendarCreator.createCalendarICS(document, month, response);
-
-        return null;
     }
-
 }
